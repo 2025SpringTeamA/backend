e3485fc063a7e6820a063a666fd4ff01b7e6e1a9
ARISA1115 <a.engine0808@gmail.com>
2025-04-27 17:20:20 +0900

    [add]各Routerをまとめる処理を追加

---
+++ app/api/api.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.api.endpoints import auth, user, session, message, emotion, favorite, generated_media
+
+api_router = APIRouter()
+
+api_router.include_router(auth.router, tags=["Auth"])
+api_router.include_router(user.router, tags=["User"])
+api_router.include_router(session.router, tags=["Session"])
+api_router.include_router(message.router, tags=["Message"])
+api_router.include_router(emotion.router, tags=["Emotion"])
+api_router.include_router(favorite.router, tags=["Favorite"])
+api_router.include_router(generated_media.router, tags=["GeneratedMedia"])
