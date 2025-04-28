bced6dde6567c1b6369008042c1a944dd94ea731
ARISA1115 <a.engine0808@gmail.com>
2025-04-29 02:44:27 +0900

    [add]認証関連のロジックを追加

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from passlib.context import CryptContext
@@ -7,27 +9,23 @@ from app.core.database import get_db
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
-# OAuth2のアクセストークンをリクエストヘッダから自動的に取得するための依存関係
+load_dotenv()
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# OAuth2のトークン受け取り用
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
-# 秘密鍵とアルゴリズムの設定
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-# パスワードのハッシュ化
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-# ハッシュ化されたパスワードと入力されたパスワードの一致確認
 def verify_password(plain_password: str, hashed_password: str) -> bool:
 
     return pwd_context.verify(plain_password, hashed_password)
 
-# JWT アクセストークンの作成
 def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
 
     to_encode = data.copy()
@@ -36,7 +34,6 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# トークンから現在のユーザー情報を取得する
 def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)) -> User:
     
     credentials_exception = HTTPException(
@@ -45,7 +42,6 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # トークンをデコードしてユーザーIDを取得
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
         if user_id is None:
@@ -53,7 +49,6 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
     except JWTError:
         raise credentials_exception
 
-    # ユーザーをデータベースから取得
     user = db.query(User).filter(User.id == int(user_id)).first()
     if user is None:
         raise credentials_exception
