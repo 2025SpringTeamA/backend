8cac3f6cc756f9df01c5fff40a5a1ab09895cdeb
ARISA1115 <a.engine0808@gmail.com>
2025-04-29 02:41:57 +0900

    [add]adminルーターをAPIに追加し、管理者機能用のエンドポイントをルーティングに組み込み

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from app.api.endpoints import auth, user, session, message, emotion, favorite, generated_media
+from app.api.endpoints import auth, admin, user, session, message, emotion, favorite, generated_media
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, tags=["Auth"])
+api_router.include_router(admin.router, tags=["admin"])
 api_router.include_router(user.router, tags=["User"])
 api_router.include_router(session.router, tags=["Session"])
 api_router.include_router(message.router, tags=["Message"])
